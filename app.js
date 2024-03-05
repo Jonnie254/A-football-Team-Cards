c0d52b05ecfc9056835b3f7429599e9767b1064f
jonnie254 <johnkamanu254@proton.me>
2024-03-05 22:04:37 +0300

    added some codde

---
+++ app.js
@@ -0,0 +1,27 @@
+/* declare the variables */
+const teamName = document.getElementById('team');
+const typeOfSport  = document.getElementById('sport');
+const worldCupYear = document.getElementById('year');
+const headCoach = document.getElementById('head-coach');
+const playerCards = document.getElementById('player-cards');
+const playersDropdownList = document.getElementById('players');
+
+/* create a new team object */
+const myFavoriteFootballTeam = {
+    team:"Argentina",
+    sport:"Football",
+    year: "1986",
+    isWorldCupWinner: true,
+    headCoach:{
+        name:"Carlos Bilardo",
+        matches: 7,
+    },
+    players:[
+        {
+            name:
+        }
+    ]
+
+    
+
+}
